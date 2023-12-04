24c13e9730c8c3af64b5c28f63fbfb8e715f7e8a
bypete-io <dev@bypete.io>
2023-12-04 13:18:22 +0000

    Extended with content collection

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,18 @@
 import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
 export async function GET(context) {
+  const posts = await getCollection("posts");
   return rss({
     title: "Astro Learner | Blog ",
     description: "My journey learning Astro",
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
+    items: posts.map((post) => ({
+      title: posts.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.slug}/`,
+    })),
     customData: `<language>en-gb</language>`,
   });
 }
