47f0c310ae386d786dcfc7b49775b8d9ff9bc7cb
bypete-io <dev@bypete.io>
2024-01-06 02:13:44 +0000

    add contact form

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,36 +5,31 @@ import preact from '@astrojs/preact';
 import mdx from '@astrojs/mdx';
 import rehypeAttrs from 'rehype-attr';
 import rehypeExternalLinks from 'rehype-external-links';
-import alpinejs from '@astrojs/alpinejs';
+import alpinejs from "@astrojs/alpinejs";
+const rehypeExternalLinksConfig = [rehypeExternalLinks, {
+  target: '_blank',
+  rel: ['noopener', 'noreferrer'],
+  content: {
+    type: 'text',
+    value: '🔗'
+  }
+}];
 
-const rehypeExternalLinksConfig = [
-    rehypeExternalLinks,
-    {
-        target: '_blank',
-        rel: ['noopener', 'noreferrer'],
-        content: { type: 'text', value: '🔗' },
-    },
-];
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://bypete.uk',
-
-    integrations: [
-        preact(),
-        mdx({
-            rehypePlugins: [rehypeAttrs, rehypeExternalLinksConfig],
-        }),
-        alpinejs(),
-        icon({
-            include: {
-                mdi: ['*'], // (Default) Loads entire Material Design Icon set
-                lucide: ['*'],
-            },
-        }),
-    ],
-    markdown: {
-        rehypePlugins: [rehypeAttrs, rehypeExternalLinksConfig],
-        remarkPlugins: [remarkReadingTime],
-    },
+  site: 'https://bypete.uk',
+  integrations: [preact(), mdx({
+    rehypePlugins: [rehypeAttrs, rehypeExternalLinksConfig]
+  }), icon({
+    include: {
+      mdi: ['*'],
+      // (Default) Loads entire Material Design Icon set
+      lucide: ['*']
+    }
+  }), alpinejs()],
+  markdown: {
+    rehypePlugins: [rehypeAttrs, rehypeExternalLinksConfig],
+    remarkPlugins: [remarkReadingTime]
+  }
 });
