83ee7c1da9487343c7827a31959ea3e078eda640
bypete-io <dev@bypete.io>
2024-04-18 11:58:52 +0100

    Fix typescript errors

--- src/entrypoint.ts
+++ src/entrypoint.ts
@@ -1,9 +1,7 @@
 import type { Alpine } from 'alpinejs'
-import intersect from '@alpinejs/intersect'
 import sticky from 'alpinejs-sticky'
 
 export default (Alpine: Alpine) => {
-    Alpine.plugin(intersect);
     Alpine.plugin(sticky);
     Alpine.store('cache', {
       banner: null,
